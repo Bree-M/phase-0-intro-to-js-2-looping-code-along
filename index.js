29afc1f1c5ed75e7ad2388db2942fe167dc4110e
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-23 03:01:58 +0300

    learning again through practice

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []; 
+  
+    for (let i = 0; i < names.length; i++) { 
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`); 
+    }
+  
+    return messages; 
+  }
+  
+  
+  console.log(writeCards(["Alice", "Bob", "Charlie"], "birthday"));
+
+  
+
+function countDown(num) {
+    if (num < 0) {
+        console.log("Please enter a positive integer");
+    return;
+}
+
+console.log(num);
+
+if(num > 0) {
+    countDown(num -1);
+}
+}
+countDown(10);
